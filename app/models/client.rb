351d18e5e9780d58ab14c650f15185d27a233837
Jerel Navarrete <navarrete.jerel@gmail.com>
2014-10-06 12:39:40 -0700

    updating associations

--- app/models/client.rb
+++ app/models/client.rb
@@ -1,2 +1,4 @@
 class Client < ActiveRecord::Base
+  has_many :appointments
+  has_many :therapists, through: :appointments
 end
--- app/models/therapist.rb
+++ app/models/therapist.rb
@@ -1,2 +1,4 @@
 class Therapist < ActiveRecord::Base
+  has_many :appointments
+  has_many :clients, through: :appointments
 end
