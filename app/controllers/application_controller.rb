4c5bb5759e1d167fe701cc7ae6d3d3792b7245b3
Jerel Navarrete <navarrete.jerel@gmail.com>
2014-10-07 11:28:20 -0700

    adding username to sign in

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,11 +1,38 @@
 class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
+  before_action :configure_permitted_parameters, if: :devise_controller?
   protect_from_forgery with: :exception
 
   include Pundit
 
   def pundit_user
     current_therapist
+  end
+
+  protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.for(:sign_up) {
+      |u| u.permit(:first_name,
+                   :last_name,
+                   :username,
+                   :email,
+                   :password,
+                   :password_confirmation,
+                   :remember_me)}
+    devise_parameter_sanitizer.for(:sign_in) {
+      |u| u.permit(:login,
+                   :username,
+                   :email,
+                   :password,
+                   :remember_me)}
+    devise_parameter_sanitizer.for(:account_update) {
+      |u| u.permit(:first_name,
+                   :last_name,
+                   :username,
+                   :email,
+                   :password,
+                   :current_password)}      
   end
 end
--- app/models/client.rb
+++ app/models/client.rb
@@ -1,9 +1,20 @@
 class Client < ActiveRecord::Base
   has_many :appointments
   has_many :therapists, through: :appointments
+  attr_accessor :login
 
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+  validates :username, :uniqueness => { :case_sensitive => false}
+
+  def self.find_for_database_authentication(warden_conditions)
+    conditions = warden_conditions.dup
+    if login = conditions.delete(:login)
+      where(conditions).where(["lower(username) = :value OR lower(email) = :value", { :value => login.downcase }]).first
+    else
+      where(conditions).first
+    end
+  end 
 end
