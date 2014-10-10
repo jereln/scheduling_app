5fdf33ca76bf59e1a68086f28c6337693fa66942
Reed Nelson-Saunders <reedns@hotmail.com>
2014-10-10 12:40:05 -0700

    email config

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -55,15 +55,6 @@ Rails.application.configure do
 
   # Enable serving of images, stylesheets, and JavaScripts from an asset server.
   # config.action_controller.asset_host = "http://assets.example.com"
-  config.action_mailer.delivery_method = :smtp
-  config.action_mailer.smtp_settings = {
-    address:              'smtp.gmail.com',
-    port:                 587,
-    domain:               'example.com',
-    user_name:            ENV['GMAIL_ACCOUNT'],
-    password:             ENV['GMAIL_PASSWORD'],
-    authentication:       'plain',
-    enable_starttls_auto: true  }
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
