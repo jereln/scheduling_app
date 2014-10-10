d311ee8039ba0377961d83ae14f9a84b4c22121b
Jerel Navarrete <navarrete.jerel@gmail.com>
2014-10-10 12:14:58 -0700

    adding date and time validations

--- app/models/appointment.rb
+++ app/models/appointment.rb
@@ -5,4 +5,22 @@ class Appointment < ActiveRecord::Base
   validates :date, presence: true
   validates :start_time, presence: true
   validates :end_time, presence: true
+  validate :reservation_valid, on: :update
+  validate :valid_block
+
+  def reservation_valid
+    if reservation_time < start_time || reservation_time > end_time
+      errors.add(:reservation_time, 'must be within the available block')
+    end
+  end
+
+  def valid_block
+    if start_time > end_time
+      errors.add(:start_time, 'must be before the start time')
+    end
+
+    if date < Time.now
+      errors.add(:date, 'must be in the future')
+    end
+  end
 end
